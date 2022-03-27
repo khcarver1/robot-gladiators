8b2308290866f8152d0d6fe13cfeb0e63b475c8b
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-03-27 11:42:06 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -53,28 +53,49 @@ var fight = function(enemyName) {
         }
     }
 }
-for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for (var i = 0; i < enemyNames.length; i++) {
+      if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+        var pickedEnemyName = enemyNames[i];
+  
+        enemyHealth = 50;
+  
+        fight(pickedEnemyName);
+      }
+      else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+    }
+    startGame();
+    endGame();
+  };
+  var endGame = function() {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } 
+    else {
+      window.alert("You've lost your robot in battle.");
+    }
+  }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+    // restart the game
+    startGame();  
+    } 
+    else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+};
+
+
 
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //    * Fight all enemy-robots
 //    * Defeat each enemy-robo
-
-
-
-function startGame() {
-
-}
-
-
-function endGame() {
-    window.prompt("would you like to play again?")
-}
-
-shop() {
-    
-}
