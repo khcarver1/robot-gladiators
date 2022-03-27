fd94401a287eba988be23dee02eb4272c4295660
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-03-26 18:50:05 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,20 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Jimothy";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 //variable for how much money the player starts with
-var playerMoney = 10;
-
-
-var fight = function() {
+var fight = function(enemyName) {
+    //fight function stmts
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -22,20 +22,20 @@ var fight = function() {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-          playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+          playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has " + enemyHealth + " health remaining."
         );
       
         // check enemy's health
         if (enemyHealth <= 0) {
-          window.alert(enemyName + " has died!");
+          window.alert(enemyNames[i] + " has died!");
         } else {
-          window.alert(enemyName + " still has " + enemyHealth + " health left.");
+          window.alert(enemyNames[i] + " still has " + enemyHealth + " health left.");
         }
       
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-          enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+          enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
       
         // check player's health
@@ -62,4 +62,12 @@ var fight = function() {
   };
 
   
-fight();
+  for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
